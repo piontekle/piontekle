8d80154b23db62c738411a1aeb3e3c173794df32
Lauren Piontek <piontekle@gmail.com>
2019-09-18 12:42:07 -0400

    Fix seq'l operatorsAliases boolean warning, sidebar margin

--- src/client/components/nav/index.jsx
+++ src/client/components/nav/index.jsx
@@ -20,11 +20,11 @@ const Header = () => (
 
 const Sidebar = () => (
   <section id="sidebar">
-    <Link className="link" to="/contact" title="Email Me"><img className="icon" src="https://live.staticflickr.com/65535/48683705156_61bc0bba51_z.jpg" alt="email-icon"/></Link>
-    <a className="link" href="https://linkedin.com/in/piontekle" title="Connect on LinkedIn" target="_blank"><img className="icon" src="https://live.staticflickr.com/65535/48683705116_7549316d28_z.jpg"  alt="linkedin-icon"/></a>
-    <a className="link" href="https://drive.google.com/file/d/1ZoS0h7HZtxJsc4K-FB_AH1xBXwVr113T/view?usp=sharing" title="My Resume" target="_blank"><img className="icon" src="https://live.staticflickr.com/65535/48689825136_95a132994e_m.jpg" alt="resume-icon"/></a>
-    <a className="link" href="https://github.com/piontekle" title="My GitHub" target="_blank"><img className="icon" src="https://live.staticflickr.com/65535/48683705136_fc7a122ab2_z.jpg"  alt="git-icon"/></a>
-    <a className="link" href="https://twitter.com/DevByLP" title="My Dev Twitter" target="_blank"><img className="icon twitter" src="https://live.staticflickr.com/65535/48692857941_87618f3f01_m.jpg"  alt="twitter-icon"/></a>
+    <Link className="link side-link" to="/contact" title="Email Me"><img className="icon" src="https://live.staticflickr.com/65535/48683705156_61bc0bba51_z.jpg" alt="email-icon"/></Link>
+    <a className="link side-link" href="https://linkedin.com/in/piontekle" title="Connect on LinkedIn" target="_blank"><img className="icon" src="https://live.staticflickr.com/65535/48683705116_7549316d28_z.jpg"  alt="linkedin-icon"/></a>
+    <a className="link side-link" href="https://drive.google.com/file/d/1ZoS0h7HZtxJsc4K-FB_AH1xBXwVr113T/view?usp=sharing" title="My Resume" target="_blank"><img className="icon" src="https://live.staticflickr.com/65535/48689825136_95a132994e_m.jpg" alt="resume-icon"/></a>
+    <a className="link side-link" href="https://github.com/piontekle" title="My GitHub" target="_blank"><img className="icon" src="https://live.staticflickr.com/65535/48683705136_fc7a122ab2_z.jpg"  alt="git-icon"/></a>
+    <a className="link side-link" href="https://twitter.com/DevByLP" title="My Dev Twitter" target="_blank"><img className="icon twitter" src="https://live.staticflickr.com/65535/48692857941_87618f3f01_m.jpg"  alt="twitter-icon"/></a>
   </section>
 )
 
